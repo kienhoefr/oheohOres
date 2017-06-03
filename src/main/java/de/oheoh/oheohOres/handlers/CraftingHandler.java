e40888de612d8da2383aab280314b1ee5addf961
oheoh <oheoh@users.noreply.github.com>
2017-06-03 15:52:31 +0200

    - Made tools craftable - Changed Link in mcmod.info to CurseForge

--- src/main/java/de/oheoh/oheohOres/handlers/CraftingHandler.java
+++ src/main/java/de/oheoh/oheohOres/handlers/CraftingHandler.java
@@ -2,11 +2,12 @@ package de.oheoh.oheohOres.handlers;
 
 import de.oheoh.oheohOres.init.OoBlocks;
 import de.oheoh.oheohOres.init.OoItems;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 /**
- * Created by oheoh on 01.06.2017 18:17 Uhr for oheohOres.
+ * Crafting Handler created by oheoh on 01.06.2017 18:17 Uhr for oheohOres.
  */
 public class CraftingHandler {
     public static void init() {
@@ -16,6 +17,13 @@ public class CraftingHandler {
         GameRegistry.addShapelessRecipe(new ItemStack(OoItems.oheriumIngot), new ItemStack(OoItems.oheriumNugget), new ItemStack(OoItems.oheriumNugget), new ItemStack(OoItems.oheriumNugget), new ItemStack(OoItems.oheriumNugget), new ItemStack(OoItems.oheriumNugget), new ItemStack(OoItems.oheriumNugget), new ItemStack(OoItems.oheriumNugget), new ItemStack(OoItems.oheriumNugget), new ItemStack(OoItems.oheriumNugget));
         GameRegistry.addShapelessRecipe(new ItemStack(OoItems.oheriumShard), new ItemStack(OoItems.oheriumNugget), new ItemStack(OoItems.oheriumNugget), new ItemStack(OoItems.oheriumNugget), new ItemStack(OoItems.oheriumNugget));
         GameRegistry.addRecipe(new ItemStack(OoItems.oheriumComponent), " S ", "SIS", " S ", 'S', OoItems.oheriumShard, 'I', OoItems.oheriumIngot);
+        GameRegistry.addRecipe(new ItemStack(OoItems.oheriumPickaxe), "CCC", " S ", " S ", 'C', OoItems.oheriumComponent, 'S', Items.STICK);
+        GameRegistry.addRecipe(new ItemStack(OoItems.oheriumSword), " C ", " C ", " S ", 'C', OoItems.oheriumComponent, 'S', Items.STICK);
+        GameRegistry.addRecipe(new ItemStack(OoItems.oheriumAxe), "CC ", "CS ", " S ", 'C', OoItems.oheriumComponent, 'S', Items.STICK);
+        GameRegistry.addRecipe(new ItemStack(OoItems.oheriumAxe), " CC", " SC", " S ", 'C', OoItems.oheriumComponent, 'S', Items.STICK);
+        GameRegistry.addRecipe(new ItemStack(OoItems.oheriumShovel), " C ", " S ", " S ", 'C', OoItems.oheriumComponent, 'S', Items.STICK);
+        GameRegistry.addRecipe(new ItemStack(OoItems.oheriumHoe), " CC", " S ", " S ", 'C', OoItems.oheriumComponent, 'S', Items.STICK);
+        GameRegistry.addRecipe(new ItemStack(OoItems.oheriumHoe), "CC ", " S ", " S ", 'C', OoItems.oheriumComponent, 'S', Items.STICK);
     }
 
 
